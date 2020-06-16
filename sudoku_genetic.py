d7a5397dd250d01be8ffabdac47aa89bab6fd8c9
hansLee-it <29248842+hansLee-it@users.noreply.github.com>
2020-06-16 16:54:14 +0900

    Update sudoku_genetic.py

--- sudoku_genetic.py
+++ sudoku_genetic.py
@@ -8,24 +8,27 @@ sudoku_problem = []
 sudoku_solve = []
 high_value = []
 high_fitness = 0
-value = [[9,0,0,8,1,2,0,0,6],[0,1,2,0,0,0,8,7,0],[6,0,0,7,9,5,0,1,0],[0,5,7,3,6,0,0,0,0],[0,0,1,0,2,0,3,0,0],[0,0,0,0,4,7,9,2,0],[0,4,0,2,5,1,0,0,3],[0,8,6,0,0,0,2,5,0],[1,0,0,6,8,3,0,0,4]]
-marking = [[[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False]]]
-order = [[[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]]
+prob = [[9,0,0,8,1,2,0,0,6],[0,1,2,0,0,0,8,7,0],[6,0,0,7,9,5,0,1,0],[0,5,7,3,6,0,0,0,0],[0,0,1,0,2,0,3,0,0],[0,0,0,0,4,7,9,2,0],[0,4,0,2,5,1,0,0,3],[0,8,6,0,0,0,2,5,0],[1,0,0,6,8,3,0,0,4]]
+value = []
 fixed = [[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False]]
 values = []
 element = [1,2,3,4,5,6,7,8,9]
 random.seed(a=None)
-posRowElements = [0,0,0,0,0,0,0,0,0]
-posColElements = [0,0,0,0,0,0,0,0,0]
-posSquareElements = [[0,0,0],[0,0,0],[0,0,0]]
+times = 0.0
+gens = 0
+n = 0
 #Genetic algorithm for sudoku.
 #Get mutation rate. Score the population of the sudoku problem. Pick high fitnesses.
 def genetic(popul,selection,mutation,max_gen):
     global values
     global value
     global high_fitness
+    global times
+    global gens
+    global n
+    global start
     fitness_values = []
-    
+    start = t.time()
     gen = 0
     #First Generation
     for i in range(popul):
@@ -37,10 +40,14 @@ def genetic(popul,selection,mutation,max_gen):
     while(gen != max_gen):
         #answerFound
         if(high_fitness==243):
-            global start
-            print(t.time()-start)
+            
+            time = t.time()-start
+            print(time)
+            times += time
             print(gen+1," Generation")
             print("Complete!")
+            gens += gen
+            n += 1
             return
         #getFitness
         for i in range(popul):
@@ -48,8 +55,7 @@ def genetic(popul,selection,mutation,max_gen):
         #select
         select(copy.deepcopy(selection),popul,copy.deepcopy(fitness_values))
         #cross
-        selected = int(popul*selection)
-        cross(popul,selection,copy.deepcopy(values[random.randrange(0,selected,1)]),copy.deepcopy(values[random.randrange(0,selected,1)]))
+        cross(popul,selection)
         #mutate
         for i in range(popul):
             values[i] = copy.deepcopy(mutate(values[i],mutation))
@@ -61,42 +67,21 @@ def genetic(popul,selection,mutation,max_gen):
     
 
     
-def cross(popul,selection,valueo,valuet):
+def cross(popul,selection):
     global values
-    insv = copy.deepcopy(valueo)
-    inst = copy.deepcopy(values)
+    selected = int(popul*selection)
+    inst = []
     while(len(inst) != popul):
+        valueo = copy.deepcopy(values[random.randint(0,selected-1)])
+        valuet = copy.deepcopy(values[random.randint(0,selected-1)])
         for i in range(9):
             for j in range(9):
-                ins = copy.deepcopy(insv[i][j])
-                insv[i][j] = copy.deepcopy(valuet[i][j])
-                if(not checkElementFlag(insv,i,j)):
-                    insv[i][j] = copy.deepcopy(ins)
-        inst.append(copy.deepcopy(insv))
+                ins = copy.deepcopy(valueo[i][j])
+                valueo[i][j] = copy.deepcopy(valuet[i][j])
+                if(not checkElementFlag(valueo,i,j)):
+                    valueo[i][j] = copy.deepcopy(ins)
+        inst.append(copy.deepcopy(valueo))
     values = copy.deepcopy(inst)
-
-
-
-
-'''
-def mutate(value,mutation):
-    mut_val = int(mutation*81)
-    same = {}
-    
-    while(mut_val != 0):
-        row = copy.deepcopy(random.randint(0,8))
-        column = copy.deepcopy(random.randint(0,8))
-        if(same.get(row) != column and not(fixed[row][column])):
-            ins = copy.deepcopy(value[row][column])
-            value[row][column] = copy.deepcopy(random.randint(1,9))
-            if(not checkElementFlag(value,row,column)):
-                value[row][column] = copy.deepcopy(ins)
-                same[row] = column
-            else:
-                same[row] = column
-                mut_val -= 1
-    return value
-'''
 
 def mutate(value,mutation):
     mut_val = int(mutation*81)
@@ -156,6 +141,15 @@ def select(selection,popul,fitness_values):
 def ready():
     global value
     global fixed
+    global prob
+    global high_fitness
+    global high_value
+    global values
+    values = []
+    high_fitness = 0
+    high_value = []
+    value = copy.deepcopy(prob)
+    
     for i in range (9):
         for j in range (9):
             if(value[i][j] != 0):
@@ -235,21 +229,6 @@ def getposElement(value,i,j):
         value[i][j] = 0
     return posv
 
-'''
-def put_element(value):
-    global fixed
-    for i in range(9):
-        for j in range(9):
-            if(not fixed[i][j]):
-                posv=getposElement(value,i,j)
-                try:
-                    value[i][j] = copy.deepcopy(posv[random.randint(0,len(posv)-1)])
-                except:
-                    pass
-    return value
-'''
-
-
 def put_element(value):
     cnt = 81
     same = {}
@@ -268,73 +247,6 @@ def put_element(value):
                 cnt -= 1
     return value
 
-
-'''
-def put_element(value):
-    global fixed
-    cnt = 81
-    while(cnt == 0):
-        element = [1,2,3,4,5,6,7,8,9]
-        min = [0,0,0,0]
-        mini = [999,999,999]
-        for i in range(9):
-            for j in range(9):
-                posRowElements[i] = 0
-                posColElements[j] = 0
-                posSquareElements[i//3][j//3] = 0
-        for i in range(9):
-            for j in range(9):
-                if(value[i][j] == 0):
-                    posRowElements[i] += 1
-                    posColElements[j] += 1
-                    posSquareElements[i//3][j//3] += 1
-        
-        for l in range(9):
-            if(posRowElements[l] < mini[0] and posRowElements[l] != 0):
-                mini[0] = posRowElements[l]
-                min[0] = l
-        for k in range(9):
-            if(posColElements[k] < mini[1] and posColElements[k] != 0):
-                mini[1] = posColElements[k]
-                min[1] = k
-        for m in range(3):
-            for n in range(3):
-                if(posSquareElements[m][n] < mini[2] and posSquareElements[m][n] != 0):
-                    mini[2] = posSquareElements[m][n]
-                    min[2] = m
-                    min[3] = n
-        if(mini[2] <= mini[0] and mini[2] <= mini[1] and mini[2] != 0):
-            for l in range(min[2]*3,(min[2]*3)+3,1):
-                for k in range(min[3]*3,(min[3]*3)+3,1):
-                    if(not fixed[l][k] or value[l][k] == 0):
-                        value[l][k] = random.randint(1,9)
-                    else: 
-                        break
-
-        elif(mini[0] <= mini[1] and mini[0] != 0):
-            for k in range(9):
-                if(not fixed[min[0]][k]):
-                    value[min[0]][k] = random.randint(1,9)
-                else:
-                    break
-                        
-        elif(mini[0] > mini[1] and mini[1] != 0):
-            for k in range(9):
-                if(not fixed[k][min[1]]):
-                    value[k][min[1]] = random.randint(1,9)
-                else:
-                    break
-        cnt -= 1
-    for i in range(9):
-        for j in range(9):
-            if(not fixed[i][j]):
-                pos = getposElement(value,i,j)
-                if(len(pos)>0):
-                    value[i][j] = copy.deepcopy(pos[random.randint(0,len(pos)-1)])
-                else:
-                    return value
-    return value
-'''
 def print_gen(popul,gen):
     global values
     print(gen+1,"gen : ")
@@ -364,12 +276,17 @@ def print_highest() :
     print()
 
 if __name__ == "__main__":
+    
 
     popul = 10
     selection = 0.5
     mutation = 0.05
     max_gen = 300000
     
-    ready()
-    genetic(popul,selection,mutation,max_gen)
-    print_highest()
+    for i in range(100):
+        ready()
+        genetic(popul,selection,mutation,max_gen)
+        print_highest()
+    
+    print("Average Time : ",times/n)
+    print("Average Generation : ",gens/n)
