bd6d8473e31b483d54431a2ef9cb59270509a22c
hansLee-it <29248842+hansLee-it@users.noreply.github.com>
2020-06-16 22:46:46 +0900

    Update sudoku_backTracking.py

--- sudoku_backTracking.py
+++ sudoku_backTracking.py
@@ -1,7 +1,10 @@
 import random
 import string
 import copy
-value = [[0,0,0,8,1,2,0,0,6],[0,1,2,0,0,0,8,7,0],[6,0,0,7,9,5,0,1,0],[0,5,7,3,6,0,0,0,0],[0,0,1,0,2,0,3,0,0],[0,0,0,0,4,7,9,2,0],[0,4,0,2,5,1,0,0,3],[0,8,6,0,0,0,2,5,0],[1,0,0,6,8,3,0,0,4]]
+from tkinter import *
+import time as t
+prob = [[0,0,0,8,1,2,0,0,6],[0,1,2,0,0,0,8,7,0],[6,0,0,7,9,5,0,1,0],[0,5,7,3,6,0,0,0,0],[0,0,1,0,2,0,3,0,0],[0,0,0,0,4,7,9,2,0],[0,4,0,2,5,1,0,0,3],[0,8,6,0,0,0,2,5,0],[1,0,0,6,8,3,0,0,4]]
+value = []
 fixed = [[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False],[False,False,False,False,False,False,False,False,False]]
 element_mark = []
 #Backtracking algorithm for sudoku.
@@ -9,10 +12,22 @@ element_mark = []
 
         
 
-def solve(v):
+def solve():
+    global prob
     global value
     global fixed
-    value = copy.deepcopy(v)
+    top = Tk()
+    top.title("Sudoku_Backtracking")                # 제목
+    top.resizable(True, True)        # 크기 고정
+    top.configure(background='white') #배경색
+    top.geometry("180x400")
+    problem = Label(top, text = getMat(prob))
+    problemlb = Label(top, text = "초기 입력 문제")
+    solving = Label(top, text = getMat(value))
+    solvinglb = Label(top, text = "풀이")
+    problemlb.pack()
+    problem.pack()
+    solvinglb.pack()
     ready()
     posElement=[]
     i=0
@@ -27,8 +42,11 @@ def solve(v):
             if(len(posElement[(i*9)+j])>0):
                 if(not fixed[i][j]):
                     value[i][j] = copy.deepcopy(posElement[(i*9)+j].pop())
-            else:
-                raise ValueError
+                    solving.destroy()
+                    solving = Label(top, text = getMat(value))
+                    solving.pack()
+                    t.sleep(0.05)
+                    top.update()
         except:
             while(True):
                 if(i == 0 and j == 0) :
@@ -51,30 +69,29 @@ def solve(v):
                     value[i][j] = 0
                     j -= 1
                     break
+                solving.destroy()
+                solving = Label(top, text = getMat(value))
+                solving.pack()
+                t.sleep(0.05)
+                top.update()
         j += 1
         if(j==9):
             i += 1
             j = 0
+    top.mainloop()
+        
+
 def ready():
+    global prob
     global value
     global fixed
+    value = copy.deepcopy(prob)
     for i in range (9):
         for j in range (9):
             if(value[i][j] != 0):
                 fixed[i][j] = True
             else:
                 fixed[i][j] = False
-    
-def checkRate():
-    global value
-    cnt = 81
-    for i in range(9):
-        for j in range(9):
-            if(value[i][j] == 0):
-                cnt -= 1
-    return cnt
-
-    
 
 def getposElement(value,i,j):
     posv = []
@@ -118,3 +135,21 @@ def print_value():
         cnt += 1
         if(cnt%9 == 0):
             print("\n")
+
+def getMat(value):
+    v = ""
+    for i in value:
+        v = v + str(i) + "\n"
+    return v
+
+if __name__ == "__main__":
+    start = t.time()
+    ready()
+    solve()
+    print(t.time()-start )
+
+
+
+
+
+
